2ed6503b244aa42f50edbc71f61850d2b5b689c2
Diego <diekus@gmail.com>
2017-02-21 19:45:58 +0000

    adds menu

---
+++ js/sdevhub.js
@@ -0,0 +1,9 @@
+let openMenu = () => {
+    let menu = document.getElementById('burger-menu');
+    menu.setAttribute('style', 'display:block;')
+};
+
+let closeMenu = () => {
+    let menu = document.getElementById('burger-menu');
+    menu.setAttribute('style', 'display:none;')
+};
