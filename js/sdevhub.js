9482c442f28c6f20387a98eb590a2e4c91cd90ef
diekus <diekus@gmail.com>
2017-02-22 16:15:27 +0000

    Burger menu open/close all css
    Avoids using Javascript to set display css attribute

--- js/sdevhub.js
+++ js/sdevhub.js
@@ -3,13 +3,16 @@
 var menu = null;
 
 document.addEventListener('DOMContentLoaded', function() {
-    menu = document.getElementById('burger-menu');   
+    menu = document.getElementById('burger-menu');
+    
 });
 
 function openMenu() {
-    menu.style.display = 'flex'
+    menu.classList.add('burger-menu_open');
+    menu.classList.remove('burger-menu_close');
 }
 
 function closeMenu() {
-    menu.style.display = 'none'
+    menu.classList.add('burger-menu_close');
+    menu.classList.remove('burger-menu_open');
 }
